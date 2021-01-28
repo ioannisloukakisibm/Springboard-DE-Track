487f92a518e0799879e4f3657a277bc76208a793
ioannisloukakisibm <62895280+ioannisloukakisibm@users.noreply.github.com>
2021-01-28 10:28:56 -0500

    Uploading the code file for the project

---
+++ OOP project (1)/banks.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self):
+        self.balance = 0
+    
+    def withdraw(self, amount):
+        self.balance -= amount
+        return self.balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+
+
+b = BankAccount().deposit(100)
+print (b)
+print ("hello")
+
